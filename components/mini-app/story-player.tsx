ba87ef5371d881a362aff3f1ae4f89d60e98f321
Danil Shustov <danzelenov@gmail.com>
2026-06-10 10:32:43 -0700

    style: update top spacing using safe area variables and adjust story player margin

--- components/mini-app/story-player.tsx
+++ components/mini-app/story-player.tsx
@@ -166,7 +166,7 @@ export function StoryPlayer({ slides, themeType, themeValue }: StoryPlayerProps)
             transformOrigin: "center center",
             position: "relative",
             flexShrink: 0,
-            marginTop: 14,
+            marginTop: 20,
           }}
         >
           {SlideCard && (
