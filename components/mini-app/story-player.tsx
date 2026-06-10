1aaf4cbbde87deb76f480192e6f9c54cb4154613
Danil Shustov <danzelenov@gmail.com>
2026-06-10 10:40:46 -0700

    fix: adjust marginTop value for story player container layout

--- components/mini-app/story-player.tsx
+++ components/mini-app/story-player.tsx
@@ -166,7 +166,7 @@ export function StoryPlayer({ slides, themeType, themeValue }: StoryPlayerProps)
             transformOrigin: "center center",
             position: "relative",
             flexShrink: 0,
-            marginTop: 24,
+            marginTop: 23,
           }}
         >
           {SlideCard && (
